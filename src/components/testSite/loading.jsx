ac65ee089114374557ade6ebe7d1991fa5edcc1f
Simon East <simon.east@cloudsensesolutions.com>
2021-03-05 00:11:36 +0000

    Customise loading screen

--- src/components/testSite/loading.jsx
+++ src/components/testSite/loading.jsx
@@ -7,7 +7,6 @@ class Loading extends React.Component {
         return (
             <div className="fixed top-0 right-0 bottom-0 left-0 flex flex-col items-center justify-center brick-bg">
                 <img src={icon} alt="Loading Icon" width="64" />
-                <span className="montserrat text-3xl mt-8 text-gray-800">The website is being built...</span>
             </div>
         )
     }
