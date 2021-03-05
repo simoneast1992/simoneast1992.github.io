00d67aee13dfdf73d8facb05c3ef7b14a12e9b58
Simon East <simon.east@cloudsensesolutions.com>
2021-03-05 00:32:12 +0000

    Form functionality part 2

--- src/components/testSite/headerForm.jsx
+++ src/components/testSite/headerForm.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 class HeaderForm extends React.Component {
     render() {
         return (
-            <form name="contact" data-netlify="true" method="POST" className="w-512px max-w-xl h-auto flex flex-col p-12 bg-white rounded-xl shadow-lg relative tracking-wide">
+            <form name="contact" method="POST" className="w-512px max-w-xl h-auto flex flex-col p-12 bg-white rounded-xl shadow-lg relative tracking-wide">
+
+                <input type="hidden" name="form-name" value="contact" />
+
                 <h3 className="pb-6 text-3xl">Get in touch</h3>
 
                 <label className="text-md mb-2 text-gray-600" for="form-name">Name<span className="text-red-500">*</span></label>
@@ -21,7 +24,9 @@ class HeaderForm extends React.Component {
                 <label className="text-md mb-2 text-gray-600" for="form-message">Message<span className="text-red-500">*</span></label>
                 <textarea className="mb-6 h-36 py-2 px-3 bg-gray-100 outline-none focus:bg-gray-100 text-gray-500 resize-none" name="message" id="form-message" required />
 
-                <input className="w-1/2 p-2 bg-red-500 text-xl text-white absolute -bottom-8 left-1/4 h-16 rounded-xl shadow-lg cursor-pointer hover:bg-red-600 outline-none transform hover:scale-105 transition duration-300 ease-in-out" type="submit" value="Submit"/>
+                <button className="w-1/2 p-2 bg-red-500 text-xl text-white absolute -bottom-8 left-1/4 h-16 rounded-xl shadow-lg cursor-pointer hover:bg-red-600 outline-none transform hover:scale-105 transition duration-300 ease-in-out" type="submit">
+                    Submit
+                </button>
             </form>
         )
     }
