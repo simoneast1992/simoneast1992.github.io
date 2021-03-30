48fd42fd9083ff633aa378b64ba18e55c675be0e
Simon East <simon.east@cloudsensesolutions.com>
2021-03-30 20:41:22 +0100

    Remove loading from dom after 5 seconds

--- src/components/testSite/loading.jsx
+++ src/components/testSite/loading.jsx
@@ -3,11 +3,27 @@ import React from 'react';
 import icon from '../../images/loading.svg';
 
 class Loading extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            rendered: true,
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(function(){
+            this.setState({rendered: false});
+        }.bind(this),5000);
+    }
+
     render() {
         return (
-            <div className="fixed top-0 right-0 bottom-0 left-0 flex flex-col items-center justify-center brick-bg">
-                <img src={icon} alt="Loading Icon" width="128" />
-            </div>
+            this.state.rendered ? (
+                <div className="fixed top-0 right-0 bottom-0 left-0 flex flex-col items-center justify-center brick-bg">
+                    <img src={icon} alt="Loading Icon" width="128" />
+                </div>
+            ) : null
         )
     }
 }
